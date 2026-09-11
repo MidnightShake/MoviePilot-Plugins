c87250a1df9d446eec68181c355cb435b2e5b8bb
MidnightShake <103491263+MidnightShake@users.noreply.github.com>
2026-09-11 16:17:18 +0800

    Downgrade plugin version from 3.0 to 2.0

--- plugins.v3/autodomainstate/__init__.py
+++ plugins.v3/autodomainstate/__init__.py
@@ -27,7 +27,7 @@ class AutoDomainState(_PluginBase):
     # 插件图标
     plugin_icon = "Chatgpt_A.png"
     # 插件版本
-    plugin_version = "3.0"
+    plugin_version = "2.0"
     # 插件作者
     plugin_author = "MidnightShake"
     # 作者主页
